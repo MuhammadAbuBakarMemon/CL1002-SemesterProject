85489ce315e5c57e6a8b8c6ca37f296b4ac57d02
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2024-12-01 09:16:04 +0500

    Update AdminPortal.c

--- AdminPortal.c
+++ AdminPortal.c
@@ -8,6 +8,15 @@ struct Admin
     int p_code;
 };
 
+struct candidate
+{
+    int election_ID;
+    char CandidateName[30];
+    char Party_name[30];
+    float votes_accumulated;
+    char seat_no[50];   // seat in the national assembly (NA-247) the candidate is contesting for in the elections
+};
+
 void AdminPortal()
 {
 
@@ -60,7 +69,55 @@ void AdminPortal()
 
     if (flag == 1)
     {
+
+        int t_entries = 0;
+
+        fptr = fopen("CandidateDetails.csv", "r");
+
+        if (fptr == NULL)
+        {
+            printf("File does not exist because you're probably making th first candidate's entry.\n");
+        }
+        else
+        {
+            fptr = fopen("candidateDetials.csv", "w");
+            t_entries = 0;
+            fclose(fptr);
+        }
+
+        int option = 0;
         
+        do 
+        {
+            printf("Welcome to the Admin Portal.\n");
+            printf("1. Add candidate\n");
+            printf("2. Disqualify candidate\n");
+            printf("3. View Live Votes tally.\n");
+            printf("4. Logout.\n");
+            scanf("%d", option);
+
+            switch (option) 
+            {
+
+                case 1 :
+                {
+                   printf("Please start providing us with the necessary details to add a candidate: \n"); 
+
+                   printf("Please enter the Election ID Number: \n");
+
+                   printf("Please enteer your candidate's name3: \n");
+
+                   printf("please enter the party the respective candidate is representoing (Mention N/A for Azad Umeedwar): \n");
+
+                   
+
+                   printf("Please enter the seat in the national assembly the candidate is contesting for: \n");
+                }
+            }
+
+
+        } while (option != 4)
+
     }
 
 
