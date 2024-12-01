6891511a42c50c1fe1cdf218b266d79901cadbd8
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2024-12-02 02:59:10 +0500

    Update VoteCaster.c

--- VoteCaster.c
+++ VoteCaster.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+
 void CastVote()
 {
-    
+
+    printf("You have suuccessfully logged in, Now proceeding towards casting your vote.\n");
+
+    struct Date t_date;
+    t_date.tm_date = 23;
+    t_date.tm_month = 12;
+    t_date.tm_year = 2024;
+
+    if (!Deadline(t_date))
+    {
+        printf("The dealine for voting has passed....\n");;
+    }
+    else
+    {
+        printf("You are just in time to cast your vote for this year's (2024)'s general elections.\n");
+
+         printf("Below are the details of all the candidates: ")
+
+
+    }
+   
 }
 
 int main(void)
